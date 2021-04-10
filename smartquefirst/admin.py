7bf4565c699d769982cc648aeef4a520f438fb6c
Dmitry Finageev <finageef@yandex.ru>
2021-04-10 19:55:08 +0000

    Base functionality

--- smartquefirst/admin.py
+++ smartquefirst/admin.py
@@ -1,13 +1,1 @@
 from django.contrib import admin
-from .models import Topic, Entry
- 
-admin.site.register(Entry)
- 
-class TopicAdmin(admin.ModelAdmin):
-	readonly_fields=('owner', )
- 
-	def save_model(self, request, obj, form, change):
-		obj.owner = request.user
-		obj.save()
- 
-admin.site.register(Topic, TopicAdmin)
