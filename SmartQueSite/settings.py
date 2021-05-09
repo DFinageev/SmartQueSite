877d486c05b6a09593255c300efa88399efa1888
Dmitry <finageef@yandex.ru>
2021-05-10 01:02:57 +0300

    Hidden key

--- SmartQueSite/settings.py
+++ SmartQueSite/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'rl6!0xwsk=eb$zq$df38nunx3*$)z9@=-y0ox455ztgmmt2tnh'
+key_file = open('key.txt', 'r')
+SECRET_KEY = key_file.read()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
